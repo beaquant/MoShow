9ad9057bea2737680edc0aa14703a1d8c736e812
melodysong <mrsong0607@gmail.com>
2018-01-23 17:51:26 +0800

    user cover picture

--- models/userProfile.go
+++ models/userProfile.go
@@ -1,17 +1,20 @@
 package models
 
 import (
+	"MoShow/utils"
 	"errors"
 	"time"
+
+	"github.com/astaxie/beego"
 
 	"github.com/jinzhu/gorm"
 )
 
 const (
-	//GenderMan .
-	GenderMan = iota
 	//GenderWoman .
-	GenderWoman
+	GenderWoman = iota
+	//GenderMan .
+	GenderMan
 )
 
 //UserProfile .
@@ -19,6 +22,7 @@ type UserProfile struct {
 	ID          uint64    `json:"user_id" gorm:"column:id;primary_key"`
 	Alias       string    `json:"alias" gorm:"column:alias"`
 	Gender      int       `json:"gender" gorm:"column:gender"`
+	CoverPic    string    `json:"cover_pic" gorm:"column:cover_pic"`
 	Description string    `json:"description" gorm:"column:description"`
 	Birthday    time.Time `json:"birthday" gorm:"column:birthday"`
 	Location    string    `json:"location" gorm:"column:location"`
@@ -26,9 +30,23 @@ type UserProfile struct {
 	Price       int       `json:"price" gorm:"column:price"`
 }
 
+//UserCoverPic .
+type UserCoverPic struct {
+}
+
 //TableName .
 func (UserProfile) TableName() string {
 	return "users"
+}
+
+//ToString .
+func (u *UserCoverPic) ToString() string {
+	str, err := utils.JSONMarshalToString(u)
+	if err != nil {
+		beego.Error(err)
+		return ""
+	}
+	return str
 }
 
 //Add .
@@ -46,6 +64,19 @@ func (u *UserProfile) Read() error {
 //Update .
 func (u *UserProfile) Update(fields map[string]interface{}) error {
 	return db.Model(u).Updates(fields).Error
+}
+
+//GetCover .
+func (u *UserProfile) GetCover() *UserCoverPic {
+	if len(u.CoverPic) > 0 {
+		ucp := &UserCoverPic{}
+		if err := utils.JSONUnMarshal(u.CoverPic, ucp); err != nil {
+			beego.Error(err)
+			return nil
+		}
+		return ucp
+	}
+	return nil
 }
 
 //AddBalance .
